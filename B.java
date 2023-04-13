ffff42ceb7e545504e2c12bb5f7042917b071288
Malleswari Boya <malleswari.boya@ibm.com>
2023-04-13 23:58:34 +0530

    Created C.java and Modified A.java, B.java

--- B.java
+++ B.java
@@ -2,6 +2,9 @@ public class B
 {
    public class void main(String args[])
    {
+      for(int i=1;i<=10;i++)
+   {
      System.out.println("Just for Demo");
     }
+}
 }
---
+++ C.java
@@ -0,0 +1,7 @@
+public class C
+{
+   public class void main(String args[])
+   {
+     System.out.println("Just for Demo purpose");
+    }
+}

740ac06f365ec334325f05e270a9e25410c3196e
Malleswari Boya <malleswari.boya@ibm.com>
2023-04-14 00:05:11 +0530

    Modified C.java

--- C.java
+++ C.java
@@ -2,6 +2,12 @@ public class C
 {
    public class void main(String args[])
    {
+     int i=1;
+     while(true)
+     {
      System.out.println("Just for Demo purpose");
+     if(i==10)
+         break;
+     i++
     }
 }
